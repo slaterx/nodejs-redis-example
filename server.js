fdcedf728ceef51a3c6e125dd2a4fd37e626e54a
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-14 08:57:12 +1000

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ var initDb = function(callback) {
         client = redis.createClient(redisURL, {no_ready_check: true});
     
     client.on("error", function (err) {
-        console.log("Redis connecting error using " + redisURL + " " + err);
+        callback("Redis connecting error using " + redisURL + " " + err);
+        return;
     });
         
     console.log('Connected to Redis at %s', redisHost, ':', redisPort);
