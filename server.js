d845b7fffe830f7d55e50376895ba54ae8babcbd
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-13 20:27:56 +1000

    Update server.js

--- server.js
+++ server.js
@@ -10,58 +10,27 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var redisPort = process.env.REDIS_SERVICE_PORT || 6379,
-    redisHost = process.env.REDIS_SERVICE_HOST || '127.0.0.1';
 
-var redis = require('redis');
-var client = redis.createClient(redisPort, redisHost, {no_ready_check: true});
 console.log('Connected to Redis');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    redisPort = process.env.REDIS_SERVICE_PORT || 6379,
+    redisHost = process.env.REDIS_SERVICE_HOST || '127.0.0.1';
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
 var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
-  if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
+    var redis = require('redis');
+    redis.createClient(redisPort, redisHost, {no_ready_check: true});
     if (err) {
       callback(err);
       return;
     }
 
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
+    console.log('Connected to Redis at: %s', redisHost);
   });
 };
 
