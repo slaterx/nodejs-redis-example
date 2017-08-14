7b83b7f2aac37f4692e25529985cd9f11c86c778
Stefano Picozzi <StefanoPicozzi@gmail.com>
2017-08-14 11:08:24 +1000

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res) {
   }
   if (db) {
     count++;
-    client.set('count', count);
+    client.set('count', count, redis.print);
     console.log('Counter = ' + client.get('count'));  
     res.render('index.html', { pageCountMessage : client.get('count'), dbInfo: dbDetails });
   } else {
